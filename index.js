b66512288d701a1e5e4cc893e231ec7604012758
Christian Fei <crifei93@gmail.com>
2016-06-12 22:41:36 +0200

    use "don't care" variable
    From: http://learnyouahaskell.com/syntax-in-functions
    > The _ means the same thing as it does in list comprehensions. It means that we really don't care what that part is, so we just write a _.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logger = morgan('dev')
 const port = process.argv[2] || 8088
 
 createServer(requestListener)
-  .listen(port, noop => console.log('-- http server listening on http://127.0.0.1:%s', port))
+  .listen(port, _ => console.log('-- http server listening on http://127.0.0.1:%s', port))
 
 function requestListener(req, res){
   let done = finalHandlerFrom(req, res)
@@ -17,6 +17,6 @@ function finalHandlerFrom(req, res){
   return (err, body = []) => {
     req
       .on('data', chunk => body.push(chunk))
-      .on('end', noop => console.log('>>>', Buffer.concat(body).toString()), res.end())
+      .on('end', _ => console.log('>>>', Buffer.concat(body).toString()), res.end())
   }
 }
